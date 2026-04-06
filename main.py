8800ab9b667d3b97fd2c782ba232eb24c8b9116a
Shishanki Sengar <shishankisengar@gmail.com>
2026-04-06 22:04:22 +0530

    add logger file

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from sensor.exception import SensorException
 import os
 import sys
 
+from sensor.logger import logging
+
+
 def test_exception():
     try:
+        logging.info("getting error")
         a=1/0
     except Exception as e:
-        # raise SensorException(e,sys)
-        raise e
+        raise SensorException(e,sys)
+        # raise e #only give output(division by zero)
 
 
 if __name__ == "__main__": # use for prevent excution on import
